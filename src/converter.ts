6d0e3f36369a7d7521393be193cd6d95b9e76706
RicoloveFeng <kakagou.mc@outlook.com>
2020-03-25 18:37:48 +0800

    :bug: fixed some layout issues, and reverted image description converting method

--- src/converter.ts
+++ src/converter.ts
@@ -35,10 +35,10 @@ export function getHeroImage(html: Document, articleType: 'CULTURE' | 'INSIDER' 
     const category = `[backcolor=Black][color=White][font="Noto Sans",sans-serif][b]${articleType}[/b][/font][/color][/backcolor][/align]`
     const img = html.getElementsByClassName('article-head__image')[0] as HTMLImageElement | undefined
     if (!img) {
-        return `[postbg]bg3.png[/postbg]\n[align=center]${category}\n`
+        return `[postbg]bg3.png[/postbg]\n\n[align=center]${category}\n`
     }
     const src = img.src
-    const ans = `[postbg]bg3.png[/postbg][align=center][img=1200,513]${resolveUrl(src)}[/img]r\n${category}\n`
+    const ans = `[postbg]bg3.png[/postbg][align=center][img=1200,513]${resolveUrl(src)}[/img]\n\n${category}\n`
 
     return ans
 }
@@ -254,10 +254,10 @@ export const converters = {
             ans = `[align=center]${ans}[/align]\n`
         } else if (ele.classList.contains('article-image-carousel__caption')) {
             // Image description
-            ans = `[align=center][b][color=Silver]${ans.replace(/\n/, '')}[/color]\n${translateMachinely(ans.replace(/\n/, ''))}[/b][/align]\n`
+            ans = `[align=center][b]${ans.replace(/\n/, '')}[/b][/align]\n`
         } else if (ele.classList.contains('video')) {
             // Video.
-            ans = '\n[media]含https的视频链接[/media]\n'
+            ans = '\n[align=center][media]含https的视频链接[/media][/align]\n'
         } else if (ele.classList.contains('quote') || ele.classList.contains('attributed-quote')) {
             ans = converters.blockquote(ele as any)
         } else if (ele.classList.contains('article-social')) {
@@ -274,7 +274,7 @@ export const converters = {
         return ''
     },
     dl: (ele: HTMLElement) => {
-        const grass = '[img=16,16]https://ooo.0o0.ooo/2017/01/30/588f60bbaaf78.png[/img]'
+        const grass = '[img=16,16]https://ooo.0o0.ooo/2017/01/30/588f60bbaaf78.png[/img][/indent][/indent]'
         // The final <dd> after converted will contains an ending comma '，'
         // So I don't add any comma before '译者'.
         const ans = `${grass}\n\n${converters.rescure(ele)}\n【本文排版借助了：[url=https://spgoding.com][color=#388d40][u]SPX[/u][/color][/url]】\n`
@@ -308,7 +308,7 @@ export const converters = {
         const prefix = '[size=6][b]'
         const suffix = '[/b][/size]'
         const inner = converters.rescure(ele)
-        const ans = `\n${prefix}[color=Silver]${inner}[/color]${suffix}\n${translateMachinely(`${prefix}${inner}${suffix}`)}\n`
+        const ans = `${prefix}[color=Silver]${inner}[/color]${suffix}\n${translateMachinely(`${prefix}${inner}${suffix}`)}\n`
 
         return ans
     },
@@ -368,7 +368,7 @@ export const converters = {
         let ans = `\n[size=2][color=Silver]${inner.replace(/#388d40/g, 'Silver')}[/color][/size]\n${translateMachinely(inner)}\n`
 
         if (ele.classList.contains('lead')) {
-            ans = `[size=4][b][size=2][color=Silver]${inner}[/color][/size][/b][/size]\n[size=4][b]${translateMachinely(inner)}[/b][/size]\n\n[size=3][color=DimGray]${authorPlaceholder}[/color][/size]`
+            ans = `[size=4][b][size=2][color=Silver]${inner}[/color][/size][/b][/size]\n[size=4][b]${translateMachinely(inner)}[/b][/size]\n\n[size=3][color=DimGray]${authorPlaceholder}[/color][/size][indent][indent]`
         }
 
         return ans
