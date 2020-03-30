bf6d55705ea7bda48263831ed582e78352fe9365
SPGoding <SPGoding@outlook.com>
2020-03-30 12:46:08 -0500

    :bug: Fix h1

--- src/converter.ts
+++ src/converter.ts
@@ -304,8 +304,8 @@ export const converters = {
         return ans
     },
     h1: async (ele: HTMLElement) => {
-        const prefix = '[size=6][b]'
-        const suffix = '[/b][/size]'
+        const prefix = '[/indent][/indent]size=6][b]'
+        const suffix = '[/b][/size][indent][indent]'
         const inner = await converters.rescure(ele)
         const ans = `${prefix}[color=Silver]${inner}[/color]${suffix}\n${translateMachinely(`${prefix}${inner}${suffix}`)}\n\n`
 
