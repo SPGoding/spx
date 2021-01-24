a979bc4240fc5e3da57d574db82393e9a2b33611
SPGoding <15277496+SPGoding@users.noreply.github.com>
2021-01-24 17:12:44 -0600

    :sparkles: Support comments

--- src/discord-bot.ts
+++ src/discord-bot.ts
@@ -27,7 +27,7 @@ export async function onMessage(config: DiscordConfig, message: Message) {
 				fs.writeFileSync(bugsPath, JSON.stringify(bugs, undefined, 4), { encoding: 'utf8' })
 				fs.appendFileSync(logPath, `${id}\t${desc}\t${member.user.tag}\t${new Date().toUTCString()}\n`, { encoding: 'utf8' })
 				await message.react('✅')
-			} else {
+			} else if (!content.startsWith('//') && !content.startsWith('#')) {
 				await message.react('❌')
 			}
 		}
